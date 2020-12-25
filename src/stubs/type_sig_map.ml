5837e8fbd5dacc8e8a31a69415026b9a26484c83
Greg Price <gnprice@gmail.com>
2020-12-25 02:02:16 -0800

    wip aha, use deriving instead

--- src/stubs/type_sig_map.ml
+++ src/stubs/type_sig_map.ml
@@ -4,30 +4,3 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *)
-
-let map_value _ _ _ = failwith "unimplemented"
-let iter_value _ _ _ = failwith "unimplemented"
-
-let map_def _ _ _ = failwith "unimplemented"
-let iter_def _ _ _ = failwith "unimplemented"
-
-let map_tparams _ _ _ = failwith "unimplemented"
-let iter_tparams _ _ _ = failwith "unimplemented"
-
-let map_annot _ _ _ = failwith "unimplemented"
-let iter_annot _ _ _ = failwith "unimplemented"
-
-let map_fun_sig _ _ _ = failwith "unimplemented"
-let iter_fun_sig _ _ _ = failwith "unimplemented"
-
-let map_class_sig _ _ _ = failwith "unimplemented"
-let iter_class_sig _ _ _ = failwith "unimplemented"
-
-let map_declare_class_sig _ _ _ = failwith "unimplemented"
-let iter_declare_class_sig _ _ _ = failwith "unimplemented"
-
-let map_interface_sig _ _ _ = failwith "unimplemented"
-let iter_interface_sig _ _ _ = failwith "unimplemented"
-
-let map_op _ _ = failwith "unimplemented"
-let iter_op _ _ = failwith "unimplemented"
