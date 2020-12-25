e9c17df43cd2d945e494bc0bbbea7d7ad5383430
Greg Price <gnprice@gmail.com>
2020-12-25 01:34:02 -0800

    fix dune: type-sig
    This becomes needed for `dune build src/flow.exe` after 34465eb6f,
    but most of the changes correspond to the earlier d07107708, which
    introduced some modules that simply weren't needed for the flow.exe
    build until then.
    
    It seems like some things are just missing, so we add to the stubs.

---
+++ src/stubs/type_sig_map.ml
@@ -0,0 +1,24 @@
+(*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *)
+
+let map_value _ _ _ = failwith "unimplemented"
+
+let map_def _ _ _ = failwith "unimplemented"
+
+let map_tparams _ _ _ = failwith "unimplemented"
+
+let map_annot _ _ _ = failwith "unimplemented"
+
+let map_fun_sig _ _ _ = failwith "unimplemented"
+
+let map_class_sig _ _ _ = failwith "unimplemented"
+
+let map_declare_class_sig _ _ _ = failwith "unimplemented"
+
+let map_interface_sig _ _ _ = failwith "unimplemented"
+
+let map_op _ _ = failwith "unimplemented"
