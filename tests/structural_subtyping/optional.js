5b31130cad0dc866b647f8d663ef786012b2d555
Greg Price <gnprice@gmail.com>
2020-12-26 23:05:54 -0800

    add a small testcase

--- tests/structural_subtyping/optional.js
+++ tests/structural_subtyping/optional.js
@@ -10,3 +10,5 @@ var test1: HasOptional = { a: "hello" }
 var test2: HasOptional = {}; // Error: missing property a
 
 var test3: HasOptional = { a: "hello", b: true }; // Error: boolean ~> number
+
+var test4: HasOptional = { a: "hello", b: 4 };
