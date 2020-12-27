50d0eac4f114e649d288c32a92e83cbfdd73ba5f
Greg Price <gnprice@gmail.com>
2020-12-26 23:35:26 -0800

    and more tests

--- tests/structural_subtyping/optional.js
+++ tests/structural_subtyping/optional.js
@@ -11,9 +11,16 @@ var test2: HasOptional = {}; // Error: missing property a
 
 var test3: HasOptional = { a: "hello", b: true }; // Error: boolean ~> number
 
-var test4: HasOptional = { a: "hello", b: 4 };
+// hmm but this seems annoying
+var test4: HasOptional = { a: "hello", b: 4 }; // Error: number ~> void
 
 var test5: HasOptional = ({ a: "hello" }: {| a: string, b?: number |});
 
-// TODO lacks error
 var test6: HasOptional = ({ a: "hello", b: 4 }: {| a: string, b: number |}); // Error
+
+
+
+// With plain object types, an object literal is accepted like this:
+var test_: { b?: number } = { b: 4 };
+// ... but not if cast to say it really does have that property:
+var test__: { b?: number } = ({ b: 4 }: { b: number });
